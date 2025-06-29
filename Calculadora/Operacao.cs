cf3845d24a7b4f000c1afae7db4c9fceb0fd347b
Icaro Vieira <icarosug@gmail.com>
2025-06-29 16:48:33 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Operacao.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacao
+{
+
+    public class OperacaoMatematica
+    {
+
+
+        internal class Soma
+        {
+            public static double calcular(double a, double b)
+            {
+                return a + b;
+            }
+        }
+
+
+
+        internal class Subtracao
+        {
+            public static double calcular(double a, double b)
+            {
+                return a - b;
+            }
+        }
+
+        internal class Multiplicacao
+        {
+            public static double calcular(double a, double b)
+            {
+                return a * b;
+            }
+        }
+
+        internal class Divisao
+        {
+            public static double calcular(double a, double b)
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException("Divisão por zero não é permitida, Pois todo número que se dividir por 0 vai dar 0");
+                }
+                return (double)a / b;
+            }
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,129 @@
+﻿using Operacao;
+using System;
+using static Operacao.OperacaoMatematica;
+using System.Globalization;
+
+
+namespace Calculadora
+{
+    public class Program
+    {
+        static OperacaoMatematica op = new OperacaoMatematica();
+        public static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                
+                Console.WriteLine("\nBem vindo a calculadora!");
+                Console.WriteLine("Escolha uma operação:");
+
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+
+                Console.WriteLine("5 - Sair");
+  
+                try
+                {
+                    int opcao = Convert.ToInt32(Console.ReadLine());
+                    if (opcao == 5)
+                    {
+                        Console.WriteLine("Saindo da calculadora...");
+                        Console.WriteLine("Obrigado por usar a Calculadora do Ícaro");
+                        return;
+                    }
+
+                    if (opcao < 1 || opcao > 4)
+                    {
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        continue;
+                    }
+
+
+                    Console.Write("Digite o Primeiro Numero: ");
+                    string entradaA = Console.ReadLine().Replace(',', '.');
+                    var a = double.Parse(entradaA, CultureInfo.InvariantCulture);
+
+                    
+                    Console.Write("Digite o Segundo Numero: ");
+                    string entradaB = Console.ReadLine().Replace(',', '.');
+                    var b = double.Parse(entradaB, CultureInfo.InvariantCulture);
+
+
+                    double resultado = 0;
+                    string operacao = "";
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            resultado = Soma.calcular(a, b);
+                            operacao = "Soma";
+                            break;
+                        case 2:
+                            resultado = Subtracao.calcular(a, b);
+                            operacao = "Subtração";
+                            break;
+                        case 3:
+                            resultado = Multiplicacao.calcular(a, b);
+                            operacao = "Multiplicação";
+                            break;
+                        case 4:
+                            resultado = Divisao.calcular(a, b);
+                            operacao = "Divisão";
+                            break;
+
+                    }
+
+                    Console.WriteLine($"Resultado da {operacao}: {resultado}");
+                    Console.WriteLine("Deseja realizar outra operação? (s/n)");
+
+                    string sair = Console.ReadLine().ToLower();
+
+                    if (sair == "sim" || sair == "não" || sair == "nao")
+                    {
+                        continuar = false;
+                        Console.WriteLine("Saindo da calculadora...");
+                    }
+                    else if (sair != "s" && sair != "sim")
+                    {
+                        Console.WriteLine("Opção inválida.");
+                    }
+
+
+                }     
+                    
+
+
+
+
+
+
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, insira um número válido.");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+                }
+                
+
+
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
